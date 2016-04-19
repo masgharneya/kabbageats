447b7975f52918f6e1805a1f9078f99789470d48
marisalaneous <me@marisalaneous.com>
2016-04-18 22:29:50 -0400

    Add Alamofire framework

--- kabbageats/MenuViewController.swift
+++ kabbageats/MenuViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class MenuViewController: UIViewController {
 
@@ -23,10 +24,33 @@ class MenuViewController: UIViewController {
     }
     
     // TODO: Add shadow for Date Navigation Bar
+    getDailyMenu()
     
-    
-    
+  }
+  
+  func getDailyMenu() {
+    Manager.request(.GET, "http://lunch.kabbage.com/api/v2/lunches/2016-04-19/").responseJSON {
+      response in
+      print(response)
+    }
   }
 
 }
+
+// Server Trust Policy Manager for Alamofire (to accept self-signed certificate, from http://stackoverflow.com/questions/31945078/how-to-connect-to-self-signed-servers-using-alamofire-1-3 )
+private var Manager : Alamofire.Manager = {
+  // Create the server trust policies
+  let serverTrustPolicies: [String: ServerTrustPolicy] = [
+    "lunch.kabbage.com": .DisableEvaluation
+  ]
+  
+  // Create custom manager
+  let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+  configuration.HTTPAdditionalHeaders = Alamofire.Manager.defaultHTTPHeaders
+  let man = Alamofire.Manager(
+    configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+    serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
+  )
+  return man
+}()
 
